8976605011cf822e68abf0ce12e9a7ba79a4edd3
Aksaev <mail-ax@ya.ru>
2023-05-28 15:43:27 +0400

    Initial commit

---
+++ src/main/java/me/aksaev/hw_basics_algorithms_2/HwBasicsAlgorithms2Application.java
@@ -0,0 +1,13 @@
+package me.aksaev.hw_basics_algorithms_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwBasicsAlgorithms2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HwBasicsAlgorithms2Application.class, args);
+    }
+
+}
---
+++ src/test/java/me/aksaev/hw_basics_algorithms_2/HwBasicsAlgorithms2ApplicationTests.java
@@ -0,0 +1,13 @@
+package me.aksaev.hw_basics_algorithms_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HwBasicsAlgorithms2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
